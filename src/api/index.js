87b672593c0663974961f853531af094024823ea
kxy <1474154683@qq.com>
2022-05-07 19:42:58 +0800

    权限管理页面

--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,18 @@ import * as trademark from './product/tradeMark';
 import * as attr from './product/attr';
 import * as sku from './product/sku';
 import * as spu from './product/spu';
+
+//权限相关的文件
+import * as user from './acl/user';
+import role from './acl/role';
+import permission from './acl/permission'
+
 export default {
     trademark,
     attr,
     sku,
-    spu
+    spu,
+    user,
+    role,
+    permission
 }

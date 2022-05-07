bdcfe104c1c3507cc7b281fc7526ac6a598c94ae
kxy <1474154683@qq.com>
2022-05-07 15:28:42 +0800

    权限管理模块组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 /* 整个项目的大框架 */
-import Layout from '@/layout'
+import Layout from "@/layout";
 
 /**
  * Note: sub-menu only appear when route children.length >= 1
@@ -32,80 +32,128 @@ import Layout from '@/layout'
  */
 export const constantRoutes = [
   {
-    path: '/login',
-    component: () => import('@/views/login/index'),
-    hidden: true
+    path: "/login",
+    component: () => import("@/views/login/index"),
+    hidden: true,
   },
 
   {
-    path: '/404',
-    component: () => import('@/views/404'),
-    hidden: true
+    path: "/404",
+    component: () => import("@/views/404"),
+    hidden: true,
   },
-
   {
-    path: '/',
+    name: "Acl",
+    path: "/acl",
     component: Layout,
-    redirect: '/dashboard',
-    children: [{
-      path: 'dashboard',
-      name: 'Dashboard',
-      component: () => import('@/views/dashboard/index'),
-      meta: { title: '首页', icon: 'dashboard' }
-    }]
+    redirect: "/acl/user/list",
+    meta: {
+      title: "权限管理",
+      icon: "el-icon-lock",
+    },
+    children: [
+      {
+        name: "User",
+        path: "user/list",
+        component: () => import("@/views/acl/user/list"),
+        meta: {
+          title: "用户管理",
+        },
+      },
+      {
+        name: "Role",
+        path: "role/list",
+        component: () => import("@/views/acl/role/list"),
+        meta: {
+          title: "角色管理",
+        },
+      },
+      {
+        name: "RoleAuth",
+        path: "role/auth/:id",
+        component: () => import("@/views/acl/role/roleAuth"),
+        meta: {
+          activeMenu: "/acl/role/list",
+          title: "角色授权",
+        },
+        hidden: true,
+      },
+      {
+        name: "Permission",
+        path: "permission/list",
+        component: () => import("@/views/acl/permission/list"),
+        meta: {
+          title: "菜单管理",
+        },
+      },
+    ],
+  },
+  {
+    path: "/",
+    component: Layout,
+    redirect: "/dashboard",
+    children: [
+      {
+        path: "dashboard",
+        name: "Dashboard",
+        component: () => import("@/views/dashboard/index"),
+        meta: { title: "首页", icon: "dashboard" },
+      },
+    ],
   },
   //整个屏幕是一个最大的组件，左侧的导航栏无论怎么切换都在Layout组件内
   //在Layout的/product路由里每写一个二级路由并保存，导航栏立即会自动生成一个二级路由的结构，样式也已经准备好
   //所以我们只需要在这里配置二级路由就可以了，这是我们所使用的后台框架帮我们准备好的便捷功能
   //二级路由的按钮结构不用我们去写
   {
-    path: '/product',
+    path: "/product",
     component: Layout,
-    name: 'Product',
-    meta: { title: '商品管理', icon: 'el-icon-goods' },
+    name: "Product",
+    meta: { title: "商品管理", icon: "el-icon-goods" },
     children: [
       {
-        path: 'trademark',
-        name: 'TradeMark',
-        component: () => import('@/views/product/tradeMark'),
-        meta: { title: '品牌管理' }
+        path: "trademark",
+        name: "TradeMark",
+        component: () => import("@/views/product/tradeMark"),
+        meta: { title: "品牌管理" },
       },
       {
-        path: 'attr',
-        name: 'Attr',
-        component: () => import('@/views/product/Attr'),
-        meta: { title: '平台属性管理' }
+        path: "attr",
+        name: "Attr",
+        component: () => import("@/views/product/Attr"),
+        meta: { title: "平台属性管理" },
       },
       {
-        path: 'spu',
-        name: 'Spu',
-        component: () => import('@/views/product/Spu'),
-        meta: { title: 'Spu管理' }
+        path: "spu",
+        name: "Spu",
+        component: () => import("@/views/product/Spu"),
+        meta: { title: "Spu管理" },
       },
       {
-        path: 'sku',
-        name: 'Sku',
-        component: () => import('@/views/product/Sku'),
-        meta: { title: 'Sku管理' }
-      }
-    ]
+        path: "sku",
+        name: "Sku",
+        component: () => import("@/views/product/Sku"),
+        meta: { title: "Sku管理" },
+      },
+    ],
   },
   // 404 page must be placed at the end !!!
-  { path: '*', redirect: '/404', hidden: true }
-]
+  { path: "*", redirect: "/404", hidden: true },
+];
 
-const createRouter = () => new Router({
-  // mode: 'history', // require service support
-  scrollBehavior: () => ({ y: 0 }),
-  routes: constantRoutes
-})
+const createRouter = () =>
+  new Router({
+    // mode: 'history', // require service support
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes,
+  });
 
-const router = createRouter()
+const router = createRouter();
 
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
 export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
+  const newRouter = createRouter();
+  router.matcher = newRouter.matcher; // reset router
 }
 
-export default router
+export default router;
